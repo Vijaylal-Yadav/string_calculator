dc7bfbddeb26c289929ccc8914e3fbea86efd8fd
Vijaylal-Yadav <vijayyadav.vy5577@gmail.com>
2025-07-29 14:06:58 +0530

    added logic for custom delimiter

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,9 +1,19 @@
+require 'byebug'
 class StringCalculator
   def add(input)
     return 0 if input.empty?
 
-    input = input.gsub("\n", ",")
-    numbers = input.split(",").map(&:to_i)
-    numbers.sum
+    input.to_i if input.size == 1
+
+    delimiter = ","
+
+    if input[0..1] == "//"
+      delimiter = input[2]
+      input = input.gsub("//", "")
+    end
+
+    input = input.gsub("\n", delimiter)
+    numbers = input.split(delimiter)
+    numbers.inject(0) { |sum, number| sum + number.to_i }
   end
 end
